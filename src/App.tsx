e0c223612a678b2759efc137cfa58df7a56a38db
Pavel Podelsnyi <46581017+podlesnyi-pavel@users.noreply.github.com>
2021-12-03 18:22:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Template } from './components/Template';
+
+const App: React.FC = () => (
+  <div className="App">
+    <h1>default</h1>
+    <Template />
+  </div>
+);
+
+export default App;
